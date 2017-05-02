c42de2209e1d27da6135f323261d6c6c2ad2180a
tzejian <akernus.lr@gmail.com>
2017-05-02 12:13:40 +0800

    Initial commit

---
+++ app/src/main/java/com/example/a127107/class_journal_p03_ps/DailyCa.java
@@ -0,0 +1,28 @@
+package com.example.a127107.class_journal_p03_ps;
+
+import java.io.Serializable;
+
+/**
+ * Created by 127107 on 2/5/2017.
+ */
+
+public class DailyCa implements Serializable {
+    private String dgGrade;
+    private String moduleCode;
+    private int week;
+
+    public String getDgGrade(){
+        return dgGrade;
+    }
+    public String getModuleCode(){
+        return moduleCode;
+    }
+    public int getWeek(){
+        return week;
+    }
+    public DailyCa(String dgGrade,String moduleCode, int week){
+        this.dgGrade = dgGrade;
+        this.moduleCode = moduleCode;
+        this.week = week;
+    }
+}
