d2708fcc66c612b7d893642b53b5399cc45ebbee
15017135 <15017135@myrp.edu.sg>
2017-05-02 16:28:40 +0800

    abcd

--- app/src/main/java/com/example/a127107/class_journal_p03_ps/InfoActivity.java
+++ app/src/main/java/com/example/a127107/class_journal_p03_ps/InfoActivity.java
@@ -13,12 +13,13 @@ import android.widget.ListView;
 import java.util.ArrayList;
 
 public class InfoActivity extends AppCompatActivity {
-    Button btnEmail,btnAdd,btnInfo;
+    Button btnEmail, btnAdd, btnInfo;
     ListView lv;
     ArrayAdapter aa;
     ArrayList<DailyCa> dailyca;
     String moduleCode;
     String moduleName;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -33,66 +34,65 @@ public class InfoActivity extends AppCompatActivity {
         // Get the String array named "info" we passed in
         String[] info = i.getStringArrayExtra("info");
         // Get the TextView object
-        moduleCode = info[0].toString();
-        moduleName = info[1].toString();
-        for(int a = 0;a<dailyca.size();a++) {
-            if(dailyca != null) {
-                Integer week = a;
-                dailyca = new ArrayList<DailyCa>();
-                dailyca.add(new DailyCa("C", moduleCode, week));
-            }
+        for (int a = 0; a < dailyca.size(); a++) {
+            moduleCode = info[0].toString();
+            moduleName = info[1].toString();
+            String DailyGrade = dailyca.get(a).getDgGrade();
+            Integer Week = dailyca.get(a).getWeek();
+
+            dailyca = new ArrayList<DailyCa>();
+            dailyca.add(new DailyCa(DailyGrade, moduleCode, Week));
+
+
+            aa = new InfoAdapter(this, R.layout.dailyrow, dailyca);
+            lv.setAdapter(aa);
+
+            Intent addIntent = new Intent(InfoActivity.this, addDailyGrade.class);
+            i.putExtra("info", info);
+            startActivity(addIntent);
+
+            btnEmail.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View arg0) {
+                    // The action you want this intent to do;
+                    // ACTION_SEND is used to indicate sending text
+                    Intent email = new Intent(Intent.ACTION_SEND);
+                    // Put essentials like email address, subject & body text
+                    email.putExtra(Intent.EXTRA_EMAIL,
+                            new String[]{"jason_lim@rp.edu.sg"});
+                    email.putExtra(Intent.EXTRA_SUBJECT,
+                            "-");
+                    email.putExtra(Intent.EXTRA_TEXT,
+                            "");
+                    // This MIME type indicates email
+                    email.setType("message/rfc822");
+                    // createChooser shows user a list of app that can handle
+                    // this MIME type, which is, email
+                    startActivity(Intent.createChooser(email,
+                            "Choose an Email client :"));
+
+                }
+            });
+
+            btnAdd.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+
+                }
+            });
+
+
+            btnInfo.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent rpIntent = new Intent(Intent.ACTION_VIEW);
+                    // Set the URL to be used.
+                    rpIntent.setData(Uri.parse("http://www.rp.edu.sg/Module_Synopses/" + moduleCode + "_" + "moduleName" + ".aspx"));
+                    startActivity(rpIntent);
+
+                }
+            });
         }
-
-
-        aa = new InfoAdapter(this, R.layout.dailyrow, dailyca);
-        lv.setAdapter(aa);
-
-                Intent addIntent = new Intent(InfoActivity.this,addDailyGrade.class);
-                i.putExtra("info",info);
-                startActivity(addIntent);
-
-        btnEmail.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View arg0) {
-                // The action you want this intent to do;
-                // ACTION_SEND is used to indicate sending text
-                Intent email = new Intent(Intent.ACTION_SEND);
-                // Put essentials like email address, subject & body text
-                email.putExtra(Intent.EXTRA_EMAIL,
-                        new String[]{"jason_lim@rp.edu.sg"});
-                email.putExtra(Intent.EXTRA_SUBJECT,
-                        "-");
-                email.putExtra(Intent.EXTRA_TEXT,
-                        "");
-                // This MIME type indicates email
-                email.setType("message/rfc822");
-                // createChooser shows user a list of app that can handle
-                // this MIME type, which is, email
-                startActivity(Intent.createChooser(email,
-                        "Choose an Email client :"));
-
-            }});
-
-        btnAdd.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-
-            }
-        });
-
-
-        btnInfo.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent rpIntent = new Intent(Intent.ACTION_VIEW);
-                // Set the URL to be used.
-                rpIntent.setData(Uri.parse("http://www.rp.edu.sg/Module_Synopses/" + moduleCode + "_" + "moduleName" + ".aspx"));
-                startActivity(rpIntent);
-
-            }
-        });
-
-
     }
 }
